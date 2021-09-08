21b60670a1645727456174c90ee63da26df9353a
RadmiloM <89080411+RadmiloM@users.noreply.github.com>
2021-09-08 14:43:44 +0200

    Update Resenje.sql

--- Nedelja 6/Sreda/Resenje.sql
+++ Nedelja 6/Sreda/Resenje.sql
@@ -1,1 +1,15 @@
+SELECT * FROM profesori
+WHERE godine > 45;
+
+SELECT godine FROM ucenici
+ORDER BY godine DESC;
+
+SELECT * FROM profesori
+WHERE predmet = 'Informatika';
+
+SELECT ime,prezime FROM ucenici
+WHERE godine > 15 AND prosek >6.5;
+
+SELECT ucenikid,ime,prezime FROM ucenici
+WHERE Length(ime) = 6;
 
